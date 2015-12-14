c2206549c4d5129454975b596bbbdb8d1c78420e
DeliciousHair <mshepit@gmail.com>
2015-12-14 16:43:27 +1100

    modified:   README.md 	modified:   google2pandas/__init__.py 	modified:   google2pandas/_panalysis_ga.py 	new file:   google2pandas/_query_parser.py

--- google2pandas/__init__.py
+++ google2pandas/__init__.py
@@ -5,5 +5,5 @@ Keywords: google analytics, pandas
 '''
 
 # bring classes directly into package namespace
-from ._panalysis_ga import GoogleAnalyticsQuery
+from ._panalysis_ga import GoogleAnalyticsQuery, GoogleMCFQuery
 
